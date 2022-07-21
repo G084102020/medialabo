87aa23f4e1ae0968ef6bbde9c58c9a019a42d7f0
G084102020 <208410@st.takushoku-u.ac.jp>
2022-07-21 15:40:27 +0900

    a

--- weather.js
+++ weather.js
@@ -45,14 +45,55 @@ let data = {
   "cod": 200
 };
 
-////////// 課題3-2 ここからプログラムを書こう
+////////// 課題3-2ここからプログラムを書こう
+let b = document.querySelector('#botan');
+b.addEventListener('click', tenki);
+function tenki() {
+let i = document.querySelector('input[name="a"]');
+    let id = Number(i.value);
+    
+      // URL を設定
+      
+  let url = 'https://www.nishita-lab.org/web-contents/jsons/openweather/'+id+'.json';
+      // 通信開始
+      axios.get(url)
+          .then(showResult)   // 通信成功
+          .catch(showError)   // 通信失敗
+          .then(finish);      // 通信の最後の処理
+  
+  
+  // 通信が成功した時の処理
+  function showResult(resp) {
+      // サーバから送られてきたデータを出力
+      let data = resp.data;
+  
+      // data が文字列型なら，オブジェクトに変換する
+      if (typeof data === 'string') {
+  
+      }
+      let t = document.querySelector('span#name');
+      t.textContent = data.name;
+      let h = document.querySelector('span#temp_max');
+      h.textContent = data.main.temp_max;
+      let u = document.querySelector('span#temp_min');
+      u.textContent = data.main.temp_min;
+  
+      // data をコンソールに出力
+      console.log(data);
+  
+      // data.x を出力
+      console.log(data.x);
+  }
+  
+  // 通信エラーが発生した時の処理
+  function showError(err) {
+      console.log(err);
+  }
+  
+  // 通信の最後にいつも実行する処理
+  function finish() {
+      console.log('Ajax 通信が終わりました');
+  }
 
-//console.log(data.name);
-//console.log(data.main.temp_min);
-//console.log(data.main.temp_max);
-let t = document.querySelector('span#tosi');
-t.textContent = tosi;
-let h = document.querySelector('span#saiki');
-h.textContent = saiki;
-let u = document.querySelector('span#teiki');
-u.textContent = teiki;
+
+}
